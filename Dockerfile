8a5506ba06c6cc0ab0d51dbf7974def8d2ac5a05
Jean-Marie TOWANOU <ofoun@yahoo.fr>
2023-11-29 20:13:37 +0100

    Add existing project files to Git

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,23 @@
-FROM openjdk:17-jdk-alpine
-VOLUME /tmp
-COPY target/*.jar app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+FROM maven:3.9.2-eclipse-temurin-17-alpine as builder
+
+COPY ./src src/
+COPY ./pom.xml pom.xml
+
+RUN mvn clean package -DskipTests
+
+FROM eclipse-temurin:17-jre-alpine
+COPY --from=builder target/*.jar app.jar
+EXPOSE 8888
+CMD ["java","-jar","app.jar"]
+
+
+
+
+
+#FROM openjdk:17-jdk-alpine
+#VOLUME /tmp
+#COPY target/*.jar app.jar
+#ENTRYPOINT ["java","-jar","/app.jar"]
+
+
 
--- src/main/java/com/ofoun/wadami/WadamiwebApplication.java
+++ src/main/java/com/ofoun/wadami/WadamiwebApplication.java
@@ -2,12 +2,26 @@ package com.ofoun.wadami;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class WadamiwebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WadamiwebApplication.class, args);
+	}
+	
+	
+	@Controller
+//	@RequestMapping("/")
+	public class HomeController {
+		@GetMapping("/")
+		public String viewHomePage() {
+						
+			return "index";
+			
+		}
 	}
 
 }
